d407e66a50332d94ab547fb160521195919c7476
Javier Gamarra <nhpatt@gmail.com>
2021-05-31 02:52:11 +0200

    2 - renderizar valor

--- src/app/solicitud/solicitud.component.ts
+++ src/app/solicitud/solicitud.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SolicitudComponent implements OnInit {
 
+  solicitud = {nombre: "Javier"};
+
   constructor() { }
 
   ngOnInit(): void {
