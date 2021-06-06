62379c4c2beb111e098cc46ea9bf920f5cb8ce82
Javier Gamarra <nhpatt@gmail.com>
2021-06-06 11:58:13 +0200

    24 - [(ngModel)]

--- src/app/solicitud/solicitud.component.ts
+++ src/app/solicitud/solicitud.component.ts
@@ -18,22 +18,8 @@ export class SolicitudComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  delete() {
-    console.log('eliminar!')
-  }
-
   enviar() {
     console.log(this.solicitud)
-  }
-
-  nombre($event: KeyboardEvent) {
-    const element = $event.target as HTMLInputElement
-    this.solicitud.nombre = element.value;
-  }
-
-  apellidos($event: KeyboardEvent) {
-    const element = $event.target as HTMLInputElement
-    this.solicitud.apellidos = element.value;
   }
 
   borrarSolicitud(solicitud: any) {
